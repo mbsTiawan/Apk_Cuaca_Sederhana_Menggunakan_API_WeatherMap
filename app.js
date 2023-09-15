91861216f6b1d9df782907cd1d00a876fbf87f60
mbsTiawan <Bagus0187@stmik-wp.ac.id>
2023-09-15 08:40:34 +0700

    first init

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const https = require("https")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", function (req, res) {
+
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function (req, res) {
+
+    const query= req.body.cityName
+    const apiKey= "f2bf64399ec4a9eee9785db3b99465b4"
+    const unit= "metric"
+    const url= `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${unit}`
+    https.get(url, function (response) {
+
+        console.log(response.statusCode)
+
+        response.on("data", function (data) {
+
+            const weatherData= JSON.parse(data)
+            const temp= weatherData.main.temp
+            const weatherDescription= weatherData.weather[0].description
+            const icon= weatherData.weather[0].icon
+            const iconURL= `https://openweathermap.org/img/wn/${icon}@2x.png`
+
+            res.write(`<p>The weather is currently is ${weatherDescription}<p>`)
+            res.write(`<h1>Temperatur in ${query} is ${temp} degrees Celcius</h1>`)
+            res.write(`<img src="${iconURL}">`)
+            res.send();
+        })
+
+    })
+
+    console.log('Post request recieved')
+})
+
+
+
+
+
+
+app.listen(3000, function () {
+
+    console.log('Server Sedang Berjalan Pada Port 3000')
+})
